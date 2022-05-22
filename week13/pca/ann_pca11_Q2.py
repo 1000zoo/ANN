89c412e9ec5b70fbaea784d630e4644b087499c5
1000zoo <cjswldn99@gmail.com>
2022-05-22 22:57:46 +0900

    pca11 / jena-climate / RNN

--- week13/pca/ann_pca11_Q2.py
+++ week13/pca/ann_pca11_Q2.py
@@ -25,7 +25,7 @@ class Data():
         self.data = copy.deepcopy(self.float_data)
         mean = self.data[:self.train_set_length].mean(axis=0)
         self.data -= mean
-        std = self.data[:self.train_set_length].std(axit=0)
+        std = self.data[:self.train_set_length].std(axis=0)
         self.data /= std
         self.mean = mean
         self.std = std
@@ -41,7 +41,7 @@ class Data():
             min_index=self.train_set_length, max_index=self.train_set_length+self.val_set_length,
             shuffle=False, step=step, batch_size=batch_size
             )
-        self.train_gen = Data.generator(
+        self.test_gen = Data.generator(
             self, lookback=lookback, delay=delay,
             min_index=self.train_set_length+self.val_set_length, max_index=None,
             shuffle=True, step=step, batch_size=batch_size
@@ -80,7 +80,7 @@ class Data():
 
 
 
-def main_ANN(ratio = [0.5, 0.25, 0.25], lookback = 1440, step = 6, delay = 144, batch_size = 128, epochs = 20):
+def main_ANN(ratio = [0.5, 0.25, 0.25], lookback = 2880, step = 6, delay = 144, batch_size = 128, epochs = 20):
     fname = "/Users/1000zoo/Documents/prog/ANN/data_files/jena_climate/jena_climate_2009_2016.csv"
     dataset = Data(fname, ratio)
     dataset.get_generators(lookback=lookback, delay=delay, batch_size=batch_size, step=step)
@@ -93,12 +93,23 @@ def main_ANN(ratio = [0.5, 0.25, 0.25], lookback = 1440, step = 6, delay = 144, 
     model.add(layers.Dense(1))
     model.compile(optimizer="RMSprop", loss="mae")
 
+    print("="*30)
+    print("="*30)
+    print("="*30)
+    print("ANN")
+    model.summary()
+
     history = model.fit_generator(
         dataset.train_gen, steps_per_epoch=dataset.train_steps, epochs=epochs,
         validation_data=dataset.val_gen, validation_steps=dataset.val_steps
     )
+    train_loss = model.evaluate_generator(dataset.train_gen, steps=dataset.val_steps)
+    test_loss = model.evaluate(dataset.test_gen, steps=dataset.test_steps)
+    print("train_loss:", train_loss)
+    print("test_loss:", test_loss)
+    return history
 
-def main_RNN(ratio = [0.5, 0.25, 0.25], lookback = 1440, step = 6, delay = 144, batch_size = 128, epochs = 20):
+def main_RNN(ratio = [0.5, 0.25, 0.25], lookback = 2880, step = 6, delay = 144, batch_size = 128, epochs = 20):
     fname = "/Users/1000zoo/Documents/prog/ANN/data_files/jena_climate/jena_climate_2009_2016.csv"
     dataset = Data(fname, ratio)
     dataset.get_generators(lookback=lookback, delay=delay, batch_size=batch_size, step=step)
@@ -106,16 +117,28 @@ def main_RNN(ratio = [0.5, 0.25, 0.25], lookback = 1440, step = 6, delay = 144, 
     from tensorflow.keras import layers, models
 
     model = models.Sequential()
-    model.add(layers.SimpleRNN(32, activation="relu"))
+    model.add(layers.SimpleRNN(32, activation="relu", input_shape=(lookback//step, dataset.data.shape[-1])))
     model.add(layers.Dense(1))
     model.compile(optimizer="RMSprop", loss="mae")
 
-    history = model.fit_generator(
-        dataset.train_gen, steps_per_epoch=dataset.train_steps, epochs=epochs,
-        validation_data=dataset.val_gen, validation_steps=dataset.val_steps
-    )
+    print("="*30)
+    print("="*30)
+    print("="*30)
+    print("RNN")
+    model.summary()
 
-def main_LSTM(ratio = [0.5, 0.25, 0.25], lookback = 1440, step = 6, delay = 144, batch_size = 128, epochs = 20):
+    # history = model.fit_generator(
+    #     dataset.train_gen, steps_per_epoch=dataset.train_steps, epochs=epochs,
+    #     validation_data=dataset.val_gen, validation_steps=dataset.val_steps
+    # )
+    # train_loss = model.evaluate_generator(dataset.train_gen, steps=dataset.val_steps)
+    # test_loss = model.evaluate_generator(dataset.test_gen, steps=dataset.val_steps)
+    # print("train_loss:", train_loss)
+    # print("test_loss:", test_loss)
+    # return history
+
+
+def main_LSTM(ratio = [0.5, 0.25, 0.25], lookback = 2880, step = 6, delay = 144, batch_size = 128, epochs = 20):
     fname = "/Users/1000zoo/Documents/prog/ANN/data_files/jena_climate/jena_climate_2009_2016.csv"
     dataset = Data(fname, ratio)
     dataset.get_generators(lookback=lookback, delay=delay, batch_size=batch_size, step=step)
@@ -123,11 +146,40 @@ def main_LSTM(ratio = [0.5, 0.25, 0.25], lookback = 1440, step = 6, delay = 144,
     from tensorflow.keras import layers, models
 
     model = models.Sequential()
-    model.add(layers.LSTM(32))
+    model.add(layers.LSTM(32,input_shape=(lookback//step, dataset.data.shape[-1])))
     model.add(layers.Dense(1))
     model.compile(optimizer="RMSprop", loss="mae")
 
-    history = model.fit_generator(
-        dataset.train_gen, steps_per_epoch=dataset.train_steps, epochs=epochs,
-        validation_data=dataset.val_gen, validation_steps=dataset.val_steps
-    )
+    print("="*30)
+    print("="*30)
+    print("="*30)
+    print("LSTM")
+    model.summary()
+
+    # history = model.fit_generator(
+    #     dataset.train_gen, steps_per_epoch=500, epochs=epochs,
+    #     validation_data=dataset.val_gen, validation_steps=dataset.val_steps
+    # )
+    # train_loss = model.evaluate_generator(dataset.train_gen, steps=dataset.val_steps)
+    # test_loss = model.evaluate_generator(dataset.test_gen, steps=dataset.val_steps)
+    # print("train_loss:", train_loss)
+    # print("test_loss:", test_loss)
+    # return history, model
+
+
+if __name__ == "__main__":
+    main_ANN()
+    # main_RNN()
+    # main_LSTM()
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(h.history['loss'])
+    plt.plot(h.history['val_loss'])
+    plt.title("ANN LOSS")
+    plt.xlabel("EPOCH")
+    plt.ylabel("LOSS")
+    plt.legend(["Training", "Validation"], loc=0)
+    plt.show()
+
+    m.summary()
